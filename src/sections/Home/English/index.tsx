a95e315c68c0f409e84ea8f0f6544d7e4e50e77e
FRAN-PC\Francisco <fran.albert.sist@gmail.com>
2024-06-24 21:08:50 -0300

    Delay for System Analyst

--- src/sections/Home/English/index.tsx
+++ src/sections/Home/English/index.tsx
@@ -1,14 +1,29 @@
 import Image from "next/image";
 import React from "react";
 import { ReactTyped } from "react-typed";
+
 function EnglishVersion() {
   return (
     <section id="about" className="w-full py-12 md:py-24 lg:py-32">
       <div className="container mx-auto px-4 md:px-6 lg:px-12 xl:px-24 grid gap-8 lg:grid-cols-2 lg:gap-12">
         <div className="space-y-4 lg:max-w-none">
-          <h1 className="text-3xl font-bold tracking-tighter sm:text-4xl md:text-5xl text-sky-900">
-            <ReactTyped strings={["Hi, I'm Francisco"]} typeSpeed={40} />
-          </h1>
+          <div className="flex items-center space-x-2">
+            <h1 className="text-3xl font-bold tracking-tighter sm:text-4xl md:text-5xl text-sky-900">
+              <ReactTyped
+                strings={["Hi, I'm Francisco"]}
+                typeSpeed={40}
+                showCursor={false}
+              />
+            </h1>
+            <h2 className="text-base font-light text-gray-700">
+              <ReactTyped
+                strings={["System Analyst"]}
+                typeSpeed={50}
+                startDelay={1500}
+                showCursor={false}
+              />
+            </h2>
+          </div>
           <p className="max-w-[700px] text-muted-foreground md:text-xl/relaxed lg:text-base/relaxed xl:text-xl/relaxed text-justify">
             I am an advanced student of Information Systems Engineering. My
             passion for technology and software development has led me to gain
@@ -20,7 +35,7 @@ function EnglishVersion() {
             src="https://mirankingtenis.s3.us-east-1.amazonaws.com/storage/avatar/Foto%20Carnet%204x4%20sin%20Fondo.png"
             width={200}
             height={350}
-            alt="Project 1"
+            alt="Francisco's Avatar"
             className="rounded-lg"
           />
         </div>
--- src/sections/Home/Spanish/index.tsx
+++ src/sections/Home/Spanish/index.tsx
@@ -7,9 +7,23 @@ function SpanishVersion() {
     <section id="about" className="w-full py-12 md:py-24 lg:py-32">
       <div className="container mx-auto px-4 md:px-6 lg:px-12 xl:px-24 grid gap-8 lg:grid-cols-2 lg:gap-12">
         <div className="space-y-4 lg:max-w-none">
-          <h1 className="text-3xl font-bold tracking-tighter sm:text-4xl md:text-5xl text-sky-900">
-            <ReactTyped strings={["Hola, soy Francisco"]} typeSpeed={40} />
-          </h1>
+          <div className="flex items-center space-x-2">
+            <h1 className="text-3xl font-bold tracking-tighter sm:text-4xl md:text-5xl text-sky-900">
+              <ReactTyped
+                strings={["Hola, soy Francisco"]}
+                typeSpeed={40}
+                showCursor={false}
+              />
+            </h1>
+            <h2 className="text-base font-light text-gray-700">
+              <ReactTyped
+                strings={["Analista de Sistemas"]}
+                typeSpeed={50}
+                startDelay={1500}
+                showCursor={false}
+              />
+            </h2>
+          </div>
           <p className="max-w-[700px] text-muted-foreground md:text-xl/relaxed lg:text-base/relaxed xl:text-xl/relaxed text-justify">
             Soy un estudiante avanzado de Ingeniería en Sistemas de Información.
             Mi pasión por la tecnología y el desarrollo de software me ha
